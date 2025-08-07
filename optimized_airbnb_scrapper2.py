9682a5200d1fbee6036040cce1096adc66e59424
Chesta1 <chestadhingra25@gmail.com>
2025-08-07 15:13:01 +0530

    Disable headless mode for debugging Selenium errors

--- optimized_airbnb_scrapper2.py
+++ optimized_airbnb_scrapper2.py
@@ -303,7 +303,8 @@ def get_driver():
         chrome_options = Options()
         
         # Basic required options
-        chrome_options.add_argument("--headless=new")
+        # chrome_options.add_argument("--headless=new")
+        chrome_options.headless = False
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
